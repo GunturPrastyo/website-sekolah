42183fb73c3aa2024a3ef50f775cf75e880508cc
prastyoguntur982-hub <prastyoguntur982@gmail.com>
2026-04-15 19:20:34 +0700

    Fix: include frontend and backend files properly

--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import VisiMisi from '../views/VisiMisi.vue'
+import FasilitasView from '../views/FasilitasView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/visi-misi',
+      name: 'visi-misi',
+      component: VisiMisi
+    },
+    {
+      path: '/fasilitas',
+      name: 'fasilitas',
+      component: FasilitasView
     }
   ]
 })
