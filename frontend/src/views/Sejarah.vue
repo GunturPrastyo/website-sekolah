2089b85b1f636159aca07d23aa4551fcc876addb
prastyoguntur982-hub <prastyoguntur982@gmail.com>
2026-04-29 18:23:15 +0700

    card no img

--- frontend/src/views/Sejarah.vue
+++ frontend/src/views/Sejarah.vue
@@ -157,7 +157,7 @@ onBeforeUnmount(() => {
             <div class="absolute inset-0 bg-blue-900/20 mix-blend-multiply"></div>
             <!-- Badge Overlay -->
             <div
-              class="absolute bottom-4 left-4 bg-white/95 dark:bg-slate-800/95 backdrop-blur-sm px-4 py-2.5 rounded-xl shadow-md border border-white/20 dark:border-slate-700"
+              class="absolute bottom-4 left-4 bg-white/95 dark:bg-slate-800/95 backdrop-blur-sm px-4 py-2.5 rounded-2xl shadow-md border border-white/20 dark:border-slate-700"
             >
               <span
                 class="text-blue-700 dark:text-blue-400 font-bold text-sm flex items-center"
@@ -250,10 +250,12 @@ onBeforeUnmount(() => {
     <section
       class="pt-6 pb-12 md:pb-24 min-h-screen relative overflow-hidden bg-gray-50 dark:bg-slate-950"
     >
-      <div class="container relative z-10 mx-auto px-0 lg:px-6 w-full max-w-full">
+      <div
+        class="container relative z-10 mx-auto px-5 sm:px-8 md:px-14 lg:px-8 w-full max-w-full"
+      >
         <!-- Bridging Title -->
         <div
-          class="mb-12 md:mb-20 text-center fade-on-scroll opacity-0 translate-y-10 transition-all duration-700 ease-out px-4 sm:px-6 pt-10 md:pt-16"
+          class="mb-10 md:mb-18 text-center fade-on-scroll opacity-0 translate-y-10 transition-all duration-700 ease-out pt-6 md:pt-12"
         >
           <div class="inline-flex items-center justify-center space-x-2 mb-4">
             <span
@@ -342,7 +344,7 @@ onBeforeUnmount(() => {
                   <!-- Badge Tahun -->
                   <div class="mb-5">
                     <div
-                      class="block w-full px-5 py-1 bg-blue-600 dark:bg-blue-500 text-white font-bold text-lg tracking-wider rounded-2xl shadow-md text-left"
+                      class="block w-full px-5 py-1 bg-blue-600 dark:bg-blue-500 text-white font-bold text-md sm:text-lg tracking-wider rounded-2xl shadow-md text-left"
                       style="font-family: 'Kalam', cursive"
                     >
                       Tahun {{ item.year }}
