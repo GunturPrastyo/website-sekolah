2e0faa7c2f5a56533dc8910c93b2ad9160834766
prastyoguntur982-hub <prastyoguntur982@gmail.com>
2026-06-19 12:40:07 +0700

    clear path image in file resource profile

--- backend/app/Http/Resources/SchoolProfileResource.php
+++ backend/app/Http/Resources/SchoolProfileResource.php
@@ -9,6 +9,14 @@ class SchoolProfileResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $data = parent::toArray($request);
+
+        if ($this->image && !str_starts_with($this->image, 'http')) {
+            // Hilangkan kata '/storage/' atau 'storage/' jika ada di awal, lalu jahit pakai url()
+            $cleanPath = str_replace(['/storage/', 'storage/'], '', $this->image);
+            $data['image'] = url('storage/' . ltrim($cleanPath, '/'));
+        }
+
+        return $data;
     }
 }
