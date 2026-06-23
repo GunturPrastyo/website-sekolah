c7278eeb219f9a55252f42bb10bade5ff9e23139
prastyoguntur982-hub <prastyoguntur982@gmail.com>
2026-06-24 06:10:09 +0700

    card ekstra

--- frontend/src/views/admin/AdminEkstrakurikuler.vue
+++ frontend/src/views/admin/AdminEkstrakurikuler.vue
@@ -664,8 +664,8 @@ const stripTags = (html) => {
       </div>
 
       <div
-        v-else
-        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
+        v-else-if="filteredEkskul.length > 0"
+        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6"
       >
         <div
           v-for="ekskul in filteredEkskul"
